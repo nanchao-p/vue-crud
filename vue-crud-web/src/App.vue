44dcd89b98619559286eb5cbad61e04ec5dfca90
nanchao_p <inanchao@gmail.com>
2021-10-27 01:09:39 +0700

    init vue form

---
+++ vue-crud-web/src/App.vue
@@ -0,0 +1,48 @@
+<template>
+  <div id="app">
+    <div class="ui inverted segment navbar">
+      <div class="ui center aligned container">
+        <div class="ui large secondary inverted pointing menu compact">
+          <router-link to="/tasks" exact class="item">
+            <i class="tasks icon"></i> Tasks
+          </router-link>
+          <router-link to="/tasks/new" exact class="item">
+            <i class="plus circle icon"></i> New
+          </router-link>
+        </div>
+      </div>
+    </div>
+    <div class="ui text container">
+      <div class="ui one column grid">
+        <div class="column">
+          <router-view></router-view>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "App",
+};
+</script>
+
+<style>
+#app > .navbar {
+  margin-bottom: 1.5rem;
+}
+input {
+  width: 300px;
+}
+.label {
+  width: 120px;
+}
+.input {
+  margin-bottom: 10px;
+}
+button.ui.button {
+  margin-top: 15px;
+  display: block;
+}
+</style>
---
+++ vue-crud-web/src/views/New.vue
@@ -0,0 +1,25 @@
+<template>
+    <div>
+        <h1>New</h1>
+        <task-form @createOrUpdate="createOrUpdate"></task-form>
+    </div>
+</template>
+
+<script>
+import taskForm from "../components/TaskForm.vue";
+import { api } from "../Helper/Helpers";
+
+export default {
+  name: "new-task",
+  components: {
+    "task-form": taskForm,
+  },
+  methods: {
+    createOrUpdate: async function (task) {
+      const res = await api.createtask(task);
+      this.flash("task created", "success");
+      this.$router.push(`/task/${res._id}`);
+    },
+  },
+};
+</script>
